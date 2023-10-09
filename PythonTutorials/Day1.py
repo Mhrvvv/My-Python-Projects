9a2b719555656a4f1c8f4e0250174bcefc587c40
Mharv Jesrael Hinayo <102411821+Mhrvvv@users.noreply.github.com>
2023-10-09 17:18:56 +0800

    Update Day1.py

--- PythonTutorials/Day1.py
+++ PythonTutorials/Day1.py
@@ -1,11 +1,13 @@
-print("Hello, World!!!")
-
 print("""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dignissim tortor eget congue molestie.
-Morbi efficitur lorem id orci tincidunt faucibus eu non felis. Donec tristique vulputate odio, a lobortis lacus accumsan non.
-Nunc interdum, erat nec ultricies venenatis, elit eros bibendum ex, eget sodales tortor metus a neque. Vivamus et egestas quam.
-In posuere lobortis felis, sit amet sodales neque accumsan eget. Nulla sagittis finibus tincidunt. 
-Nullam dapibus, mauris vitae euismod ullamcorper, nibh risus facilisis nisl, vitae venenatis leo odio ut sapien.
-Sed dictum feugiat diam, et suscipit turpis viverra vitae. Sed finibus elit in lorem euismod cursus. 
-Etiam commodo vel ante et ultricies. Maecenas posuere at orci sit amet feugiat.
+Mharv
+October 09, 2023
+I am signing up for Replit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone whenever I happen to be. No excuses
+for not coding from the middle of a field!
 """)
+print("I am feeling 😎")
+print("You can follow my progress at replit.com/@Mhrvvv")
