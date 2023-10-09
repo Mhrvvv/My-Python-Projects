6ec0b1e301b4fc21570eacbbf86b03d312e04941
Mharv Jesrael Hinayo <102411821+Mhrvvv@users.noreply.github.com>
2023-10-09 17:39:33 +0800

    Create Day2.py

---
+++ PythonTutorials/Day2.py
@@ -0,0 +1,10 @@
+#input variables day 2 Challenge
+myName = input("What's your name? ")
+myFavFood = input("What's your favorite food? ")
+myFavMusic = input("What's your favorite music? ")
+myAddress = input("Where do you live? ")
+
+print("You are " + myName)
+print("You're probably hungry for " + myFavFood)
+print("and you're definitely getting your groove on to " + myFavMusic)
+print("Living in the amazing " + myAddress)
