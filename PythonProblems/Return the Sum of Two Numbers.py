f84a7bb64959d17e6c6f4105317cd5953061ee10
Mharv Jesrael Hinayo <102411821+Mhrvvv@users.noreply.github.com>
2023-10-10 09:35:55 +0800

    Create Return the Sum of Two Numbers.py

---
+++ PythonProblems/Return the Sum of Two Numbers.py
@@ -0,0 +1,8 @@
+#Sum of two integers
+def addition(x, y):
+    return x + y
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+print(num1, "+", num2, "=", addition(num1, num2))
