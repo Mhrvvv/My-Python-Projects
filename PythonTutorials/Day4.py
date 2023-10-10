70752c8373100e0e41292e13f42d02121969fb7b
Mharv Jesrael Hinayo <102411821+Mhrvvv@users.noreply.github.com>
2023-10-10 08:48:06 +0800

    Create Day4.py

---
+++ PythonTutorials/Day4.py
@@ -0,0 +1,9 @@
+#Concatination
+
+nameFood = input("Name a food: ")
+namePlant = input("Name a type of plant: ")
+nameCooking = input("Name a method of cooking: ")
+wordDescribe = input("What word describe burned food? ")
+nameDIY = input("Name a DIY item: ")
+print("\nMENU")
+print(nameFood, "with", wordDescribe, namePlant, "on a bed of", nameDIY, "by using the method", nameCooking)
